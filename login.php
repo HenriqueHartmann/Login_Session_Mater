609a2a12964a9bfde8f2e8454f9645068c387454
HenriqueHartmann <alemaohartmann@gmail.com>
2017-06-29 11:06:18 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+	<title>Login</title>
+</head>
+<body>
+	<div class="form-login">
+	<form method="post" action="verifica_usuario.php">
+			<input type="text" name="login" placeholder="login">
+			<br />
+			<input type="password" name="senha" placeholder="senha">
+			<br />
+			<input type="submit" name="login_form" value="Acessar">
+	</form>
+	</div>
+</body>
+</html>
